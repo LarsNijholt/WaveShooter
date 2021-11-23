fb8add2cb3ee9424bb034f37e187c697e97e55c2
LarsNijholt <61054131+LarsNijholt@users.noreply.github.com>
2021-11-23 23:59:50 +0100

    Upload of C++ files

---
+++ TwinShooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TwinShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinShooter, "TwinShooter" );
